86d75172a4a03b6b92e1885f61511279ba594e66
MALIKCRASH✅️ <168739286+malpl@users.noreply.github.com>
2024-09-24 06:05:28 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,19 +9,19 @@ global.socialm = "GitHub: GlobalTechInfo" //ur github or insta name
 global.location = "Pakistan, Karachi, Karachi" //ur location
 
 //new
-global.botname = '© 𝐆𝐋𝐎𝐁𝐀𝐋-𝐌𝐃' //ur bot name
-global.ownernumber = ['923444844060'] //ur owner number, dont add more than one
-global.ownername = '© GlobalTechInfo' //ur owner name
-global.websitex = "https://www.youtube.com/@GlobalTechInfo"
-global.wagc = "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/GlobalTechInfo/GLOBAL-MD' //script link
+global.botname = '『mλɫᏐꝅ c℟λ₷h』🥶' //ur bot name
+global.ownernumber = ['201505925867'] //ur owner number, dont add more than one
+global.ownername = '『mλɫᏐꝅ c℟λ₷h』🥶' //ur owner name
+global.websitex = "https://youtube.com/@malik-b1k?si=4toY_y8KLqnWkPnw"
+global.wagc = "https://whatsapp.com/channel/0029Vanq4erBKfi8V2e0cI1P"
+global.themeemoji = '🥶'
+global.wm = "『mλɫᏐꝅ c℟λ₷h』🥶."
+global.botscript = 'https://youtube.com/@malik-b1k?si=4toY_y8KLqnWkPnw' //script link
 global.packname = "Sticker By"
-global.author = "GlobalTechInfo\n\n+923444844060"
-global.creator = "923444844060@s.whatsapp.net"
+global.author = "『mλɫᏐꝅ c℟λ₷h』🥶\n\n+201030825867"
+global.creator = "201030825867@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["923444844060"] // Premium User
+global.premium = ["201505925867"] // Premium User
 
 //channel id
 global.xchannel = {
